40c9062338dee26af8c20d49298ce04ac5aafe6c
Michelle Lau <meeshklau@gmail.com>
2022-06-14 13:08:53 -1000

    commit tree 62eb98742c79155ec70bd7ffba286c716f310244

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function handleFocus(e) {
+        if (e.type === 'focus') {
+            console.log("Good!")
+        } 
+        else if (e.type === "blur") {
+            console.log("Hey! Eyes on me!")
+        }
+        }
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleFocus}>"Eyes on me"</button>
+    )
+}
+
+
+export default EyesOnMe
